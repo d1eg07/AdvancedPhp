4529abd0acba583e489067d3da57faae886c7e5d
Diego <izayakazuto97@gmail.com>
2020-02-01 11:20:01 -0300

    Create listas.php

---
+++ listas.php
@@ -0,0 +1,7 @@
+<?php 
+
+
+$idades = [21, 23, 19, 16, 20, 40]; // cria array de idades.
+$umaIdade = $idades[3]; //armazena o índice 3 na variável $umaIdade.
+
+echo $umaIdade; //exibe a variavel $umaIdade de acordo com o índice do array.
