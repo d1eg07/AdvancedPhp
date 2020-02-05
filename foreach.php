ec4fbeb407e01bf6a17982db505f842c86dd9f93
Diego <izayakazuto97@gmail.com>
2020-02-04 22:24:13 -0300

    Update foreach.php

--- foreach.php
+++ foreach.php
@@ -1,31 +1,25 @@
 <?php
 
-'123.456.789-10' => [
-    'titular' => 'Diego',
-    'saldo' => 2600
-],
-
-'123.456.789-11' => [
-    'titular' => 'Carol',
-    'saldo' => 1000
-],
-'123.256.789-12' => [
-'titular' => 'Dick',
-'saldo' => 10000
-]
+$contasCorrentes = [
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    '123.456.689-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
+    ]
 ];
 
-$contasCorrentes[] = [
-'titular' => 'Bruce',
-'saldo' => 2000
+$contasCorrentes['123.258.852-12'] = [
+    'titular' => 'Claudia',
+    'saldo' => 2000
 ];
-
-$contasCorrentes[] = [
-'titular' => 'Bruce 2',
-'saldo' => 2002
-];
-
 
 foreach ($contasCorrentes as $cpf => $conta) {
-echo $cpf . " " . $conta['titular'] . PHP_EOL;
+    echo $cpf . " " . $conta['titular'] . PHP_EOL;
 }
