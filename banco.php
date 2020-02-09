ecec28f78aa773c8c558030bcd3ec10be408c95c
Diego <izayakazuto97@gmail.com>
2020-02-09 12:15:39 -0300

    Update banco.php

--- banco.php
+++ banco.php
@@ -1,13 +1,13 @@
 <?php
 
-require 'funcoes.php'; //incluindo arquivos de função 
+require_once 'funcoes.php';
 
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
-    '123.456.789-11' => [
+    '123.456.689-11' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ],
@@ -17,18 +17,23 @@ $contasCorrentes = [
     ]
 ];
 
+$contasCorrentes['123.456.789-10'] = sacar(
+    $contasCorrentes['123.456.789-10'],
+    500
+);
 
-if (500 > $contasCorrentes['123.456.789-10']['saldo']) { 
-    exibeMensagem("Você não pode sacar este valor"); //Chamada da função exibe mensagem
-} else
-    $contasCorrentes['123.456.789-10']['saldo'] -= 500;
+$contasCorrentes['123.456.689-11'] = sacar(
+    $contasCorrentes['123.456.689-11'],
+    200
+);
 
-
-if (500 > $contasCorrentes['123.456.789-11']['saldo']) {
-    exibeMensagem("Você não pode sacar este valor"); //Chamada da função exibe mensagem
-} else
-    $contasCorrentes['123.456.789-11']['saldo'] -= 500;
+$contasCorrentes['123.256.789-12'] = depositar(
+    $contasCorrentes['123.256.789-12'],
+    900
+);
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']); //Chamada da função exibe mensagem
+    exibeMensagem(
+        "$cpf {$conta['titular']} {$conta['titular']}"
+    );
 }
