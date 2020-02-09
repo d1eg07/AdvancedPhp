2350433ea438c78e88444eb6fcd92e5d95d6a7d0
Diego <izayakazuto97@gmail.com>
2020-02-09 13:06:36 -0300

    Update banco.php

--- banco.php
+++ banco.php
@@ -32,8 +32,29 @@ $contasCorrentes['123.256.789-12'] = depositar(
     900
 );
 
-echo "<ul>";
-foreach ($contasCorrentes as $cpf => $conta) {
-    exibeConta($conta);
-}
-echo "</ul>";
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas correntes</h1>
+
+    <dl>
+        <?php foreach($contasCorrentes as $cpf => $conta) { ?>
+        <dt>
+            <h3><?= $conta['titular']; ?> - <?= $cpf; ?></h3>
+        </dt>
+        <dd>
+            Saldo: <?= $conta['saldo']; ?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
