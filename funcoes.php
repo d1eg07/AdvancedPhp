8a9850a0b580016577fa0bcfcc767f344e646680
Diego <izayakazuto97@gmail.com>
2020-02-09 12:11:21 -0300

    Create funcoes.php

---
+++ funcoes.php
@@ -0,0 +1,26 @@
+<?php
+
+function exibeMensagem(string $mensagem){
+    echo $mensagem . PHP_EOL;
+}
+
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não pode sacar este valor");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+function depositar(array $conta, float $valorADepositar): array {
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depósitos precisam ser positivos");
+    }
+
+    return $conta;
+}
